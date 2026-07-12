c6e5978d9d0ed37863ec31a1ad03b770eb6f4491
DevopsBhaskar <bhaskara.rao.dev@gmail.com>
2026-07-12 20:06:30 +0530

    Update

---
+++ provisioner-local-exec/ec2.tf
@@ -0,0 +1,43 @@
+resource "aws_instance" "terraform" {
+    ami = "ami-0220d79f3f480ecf5"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.terrall.id]
+    tags = {
+        Name = "terraform-1"
+        Terraform = "true" # saying that creating instance using terraform
+    }
+
+     provisioner "local-exec"{
+      command = "echo ${self.private_ip} > inventory"
+      on_failure = continue
+    }
+
+    provisioner "local-exec"{
+      command = "echo Instance is destroyed"
+      when    = destroy
+    }
+}
+
+
+resource "aws_security_group" "terrall" {
+  name   = "terraform-all"
+
+  egress {
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+
+  ingress {
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+
+  tags = {
+    Name = "ter-all"
+  }
+
+}
